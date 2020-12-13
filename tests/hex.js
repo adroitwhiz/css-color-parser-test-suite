3561b1a8bff745b733c0ee1c8e062048817a19e9
adroitwhiz <adroitwhiz@protonmail.com>
2020-12-13 17:38:03 -0500

    add tests for caps/mixed hex strings

--- tests/hex.js
+++ tests/hex.js
@@ -5,6 +5,9 @@ const tests = test => {
     test('#f00c', {model: 'rgb', value: [255, 0, 0, 0.8]}, '4-digit hex with alpha', {isLevel4: true});
     test('#ff0000cc', {model: 'rgb', value: [255, 0, 0, 0.8]}, '8-digit hex with alpha', {isLevel4: true});
 
+    test('#FF0000', {model: 'rgb', value: [255, 0, 0, 1]}, 'uppercase hex');
+    test('#Ff0000', {model: 'rgb', value: [255, 0, 0, 1]}, 'mixed-case hex');
+
     test('#ff000', null, '5-digit hex');
     test('#ffz000', null, 'hex with invalid character replaced');
     test('#fffz000', null, 'hex with invalid character inserted');
